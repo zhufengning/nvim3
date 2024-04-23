2fcadde6cef580bdd877d0a055d86c474ac3121a
Fengning Zhu <renhaihao@outlook.com>
2024-04-23 15:37:45 +0800

    init

---
+++ lua/plugins/my.lua
@@ -0,0 +1,11 @@
+return {
+  { "simrat39/rust-tools.nvim" },
+  { "mini.pairs", enabled = false },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-latte",
+    },
+  },
+}
