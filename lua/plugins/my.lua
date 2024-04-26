2bc66abe22bb6a7ef51c04da694233cde7b9c410
Fengning Zhu <renhaihao@outlook.com>
2024-04-26 19:12:02 +0800

    py lsp

--- lua/plugins/my.lua
+++ lua/plugins/my.lua
@@ -8,4 +8,35 @@ return {
       colorscheme = "catppuccin-latte",
     },
   },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        pyright = {
+          enabled = true,
+        },
+        basedpyright = {
+          enabled = false,
+        },
+        ruff = {
+          enabled = false,
+          autostart = false,
+        },
+        ruff_lsp = {
+          enabled = false,
+          autostart = false,
+        },
+      },
+      setup = {
+        ruff = function()
+          LazyVim.lsp.on_attach(function(client, _)
+            if client.name == "ruff" then
+              -- Disable hover in favor of Pyright
+              client.server_capabilities.hoverProvider = false
+            end
+          end)
+        end,
+      },
+    },
+  },
 }
